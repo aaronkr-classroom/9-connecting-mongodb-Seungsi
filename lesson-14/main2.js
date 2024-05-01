448b402892d0bd49f08bacd6f9ee146bb0e1e9bb
Seungsi <117188786+Seungsi@users.noreply.github.com>
2024-05-01 02:32:02 +0000

    1314끝!

--- lesson-14/main2.js
+++ lesson-14/main2.js
@@ -6,19 +6,29 @@ const port = 3000,
   layouts = require("express-ejs-layouts"),
   homeController = require("./controllers/homeController"),
   errorController = require("./controllers/errorController"),
-  // @TODO: Subscriber 모델 가져오기
+  Subscirber = require("./models/subscriber"),
   app = express();
 
 /**
  * @TODO: Listing 14.1 (p. 205)
  * Mongoose를 사용한 MongoDB 연결
  */
+const mongoose = require("mongoose");
 
+mongoose.connect(
+  "mongodb+srv://seungsi:TNDLgbfZzRNZxP0L@ut-node.8n0krvz.mongodb.net/?retryWrites=true&w=majority&appName=ut-node/ut-node",// URL
+ {useNewUrlParser: true}
+);
+
+const db = mongoose.connection;
 
 /**
  * @TODO: Listing 14.2 (p. 206)
  * 데이터베이스 연결 이벤트 처리
  */
+db.once("open", () => {
+  console.log("DB connected!");
+})
 
 
 /**
